81ba4ed8b56a725fc93430f4f57a81c069351e46
Brennez <tchalisantos40@gmail.com>
2021-09-25 15:50:44 -0300

    front funfa

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import GlobalStyle from "./Styles/global";
 // import Register from "./Pages/Register-page"
 // import Login from "./Pages/Login-page";
-// import Home from "./Pages/Home-page"
-import RegisterBook from "./Pages/Register-Book";
+import Home from "./Pages/Home-page";
+// import RegisterBook from "./Pages/Register-Book";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       {/* <Login/> */}
-      {/* <Home/> */}
-      <RegisterBook />
+      <Home />
+      {/* <RegisterBook /> */}
     </>
   );
 }
