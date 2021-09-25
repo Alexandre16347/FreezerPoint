6b9ecc0f6c7f25d5b78d4abe499e7e8f8625409c
Brennez <tchalisantos40@gmail.com>
2021-09-25 15:29:43 -0300

    pfv funcione

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import GlobalStyle from "./Styles/global";
+// import Register from "./Pages/Register-page"
 // import Login from "./Pages/Login-page";
-import Home from "./Pages/Home-page";
+// import Home from "./Pages/Home-page"
+import RegisterBook from "./Pages/Register-Book";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       {/* <Login/> */}
-      <Home />
+      {/* <Home/> */}
+      <RegisterBook />
     </>
   );
 }
---
+++ Frontend/src/Pages/Register-Book/index.js
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import { Container, ContentForm, Image, Page } from "./styles";
+import logo from "../../Assets/logo.svg";
+import {Form} from "@unform/web"
+import Input from "../../components/input";
+
+function RegisterBook() {
+  
+  const formularioReferencia = useRef(null)
+
+  const submeterFormulario = (data) =>{
+    console.log(data);
+  }
+
+  return (
+    <Page>
+    <div className= "logo">
+        <img src={logo} alt="logo" />
+      </div>
+
+    <Container>
+      <ContentForm>
+        <h1 className="title">Cadastre um livro</h1>
+        <Form ref= {formularioReferencia} onSubmit= {submeterFormulario}>
+          <h2>Título</h2>
+          <Input name= "nome" type="text" placeholder="Harry Potter" />
+          <h2>Autor</h2>
+          <Input name= "autor" type="text" placeholder= "J. K. Rowling"/>
+          <h2>Categoria</h2>
+          <Input name= "categoria" type="text" placeholder="Livro" />
+          <h2>Gênero</h2>
+          <Input name= "genero" type="text" placeholder="Fantasia" />
+          <h2>Edição</h2>
+          <Input name= "edicao" type="number" placeholder="2008" />
+          <h2>Sinopse</h2>
+          <Input name= "sinopse" type= "text" placeholder= "Uma breve descrição sobre o livro"></Input>
+
+          <button type="submit"> Cadastrar </button>
+        </Form>
+      </ContentForm>
+      <Image> </Image>
+    </Container>
+    </Page>
+  );
+}
+
+export default RegisterBook;
